89ddfba8e0f786ea2d92c8d06acd44cd7737f7fe
Bruno Silva <s.bruno.12@hotmail.com>
2024-02-08 09:59:57 -0300

    first commit

---
+++ src/main/java/anotacao/ColunaTabela.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package anotacao;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used to mark a field as a database column.
+ * 
+ * @author rodrigo.pires
+ *
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ColunaTabela {
+
+	String dbName();
+	
+	String setJavaName();
+	
+	// Novos campos adicionados para mapear a tabela no banco de dados
+	// Adicione novos campos conforme necessário
+	String getJavaName() default ""; // Nome do método getter (ex: getEmail)
+	String columnType() default ""; // Tipo da coluna no banco de dados (ex: VARCHAR, INTEGER, etc.)
+	int length() default 255; // Comprimento da coluna no banco de dados
+	boolean isPrimaryKey() default false; // Indica se a coluna é chave primária
+	boolean isForeignKey() default false; // Indica se a coluna é chave estrangeira
+}
---
+++ src/main/java/br/com/rpires/dao/factory/ProdutoFactory.java
@@ -0,0 +1,25 @@
+/**
+ * Classe responsável por converter ResultSet em objetos Produto.
+ * 
+ */
+package br.com.rpires.dao.factory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.rpires.domain.Produto;
+
+public class ProdutoFactory {
+
+    public static Produto convert(ResultSet rs) throws SQLException {
+        Produto prod = new Produto();
+        prod.setId(rs.getLong("ID_PRODUTO"));
+        prod.setCodigo(rs.getString("CODIGO"));
+        prod.setNome(rs.getString("NOME"));
+        prod.setDescricao(rs.getString("DESCRICAO"));
+        prod.setValor(rs.getBigDecimal("VALOR"));
+        // Adicione o novo campo quantidade
+        prod.setQuantidade(rs.getInt("QUANTIDADE"));
+        return prod;
+    }
+}
